b7a3099a7a556761df01dff8ebba43b9cea24801
Avinash Malhotra <avimalhotra505@gmail.com>
2020-04-11 20:32:47 +0530

    Path Module

---
+++ src/path.js
@@ -0,0 +1,22 @@
+const path=require('path');
+
+
+//console.log(path.normalize('src'));
+//console.log(path.normalize('./src'));
+//console.log(path.normalize('.//src'));
+//console.log(path.normalize('src/../'));
+
+//console.log(path.basename('blog/technology/aero.txt','.txt'));
+
+//console.log(path.dirname('./src/views/hello.js'));
+//console.log(path.extname('test'))
+
+
+//console.log(path.resolve('src/assets'));
+//console.log(path.resolve(__filename))
+//console.log(path.resolve('views'));
+
+//console.log(path.resolve('src/views','static'));
+
+console.log(path.join("src","pics","../"))
+
